29f01d74b077cf98f4297606dab9ec02bdba6031
liucan <liucan@ismartv.cn>
2017-03-02 15:27:14 +0800

    专题包

---
+++ subject/src/main/java/tv/ismar/subject/SportSubjectFragment.java
@@ -0,0 +1,21 @@
+package tv.ismar.subject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liucan on 2017/3/1.
+ */
+
+public class SportSubjectFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+}
---
+++ subject/src/main/java/tv/ismar/subject/SubjectActivity.java
@@ -0,0 +1,17 @@
+package tv.ismar.subject;
+
+import android.os.Bundle;
+
+import tv.ismar.app.BaseActivity;
+
+/**
+ * Created by liucan on 2017/3/1.
+ */
+
+public class SubjectActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
