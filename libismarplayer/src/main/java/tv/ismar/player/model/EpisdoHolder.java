0221d7d361e2554dae9b5f3e995529a54e9a4b02
liucan <liucan@ismartv.cn>
2017-06-13 15:58:49 +0800

    fix bug 3388

--- libismarplayer/src/main/java/tv/ismar/player/model/EpisdoHolder.java
+++ libismarplayer/src/main/java/tv/ismar/player/model/EpisdoHolder.java
@@ -2,6 +2,7 @@ package tv.ismar.player.model;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.qiyi.tvapi.tv2.model.Episode;
@@ -15,8 +16,10 @@ import tv.ismar.player.R;
 public class EpisdoHolder {
     public TextView textView;
     public ImageView imageView;
+    public LinearLayout list_item;
     public EpisdoHolder(View itemView){
         textView= (TextView) itemView.findViewById(R.id.episode_pk);
         imageView= (ImageView) itemView.findViewById(R.id.select_image);
+        list_item= (LinearLayout) itemView.findViewById(R.id.episode_list_item);
     }
 }
