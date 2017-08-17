56396a2cf12469b51073f176b1420b81dd7d13e2
koujia <koujia@ismartv.cn>
2017-08-17 17:57:31 +0800

    修改筛选页界面

--- listpage/src/main/java/tv/ismar/channel/FilterActivity.java
+++ listpage/src/main/java/tv/ismar/channel/FilterActivity.java
@@ -4,7 +4,10 @@ package tv.ismar.channel;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.KeyEvent;
@@ -12,6 +15,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RadioButton;
@@ -32,6 +36,8 @@ import tv.ismar.app.core.PageIntent;
 import tv.ismar.app.core.Source;
 import tv.ismar.app.entity.Item;
 import tv.ismar.app.entity.ItemList;
+import tv.ismar.app.entity.Section;
+import tv.ismar.app.entity.SectionList;
 import tv.ismar.app.models.FilterConditions;
 import tv.ismar.app.ui.adapter.OnItemClickListener;
 import tv.ismar.app.ui.adapter.OnItemFocusedListener;
@@ -46,7 +52,7 @@ import tv.ismar.view.FilterConditionGroupView;
 public class FilterActivity extends BaseActivity implements View.OnClickListener, View.OnHoverListener, View.OnFocusChangeListener {
 
     private TextView filter_title;
-    private Button filter_tab;
+    private RadioButton filter_tab;
     private LinearLayout filter_checked_conditiion;
     private MyRecyclerView poster_recyclerview;
     private LinearLayout filter_conditions;
@@ -64,6 +70,8 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
     private int spanCount;
     private boolean canScroll=true;
     private boolean isFocused;
+    private RadioGroup section_group;
+    private View current_tab_arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,7 +101,9 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
 
     private void initView() {
         filter_title = (TextView) findViewById(R.id.filter_title);
-        filter_tab = (Button) findViewById(R.id.filter_tab);
+        section_group = (RadioGroup) findViewById(R.id.section_group);
+        filter_tab = (RadioButton) findViewById(R.id.filter_tab);
+        current_tab_arrow = findViewById(R.id.current_tab_arrow);
         filter_checked_conditiion = (LinearLayout) findViewById(R.id.filter_checked_conditiion);
         poster_recyclerview = (MyRecyclerView) findViewById(R.id.poster_recyclerview);
         poster_recyclerview.setHasFixedSize(true);
@@ -114,6 +124,13 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
         poster_recyclerview.setLayoutManager(new FocusGridLayoutManager(this, spanCount));
         filter_tab.setOnClickListener(this);
         filter_tab.setOnHoverListener(this);
+        filter_tab.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    filter();
+            }
+        });
         filter_arrow_up.setOnClickListener(this);
         filter_arrow_down.setOnClickListener(this);
         filter_arrow_up.setOnHoverListener(this);
@@ -155,7 +172,103 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
 
     private void initData() {
         fetchFilterCondition(channel);
+        fetchChannelSection(channel);
         filter_title.setText(title);
+    }
+
+    //请求list分类tab
+    private void fetchChannelSection(String channel) {
+        mSkyService.getSections(channel)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseObserver<SectionList>() {
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onNext(SectionList sections) {
+                        fillSections(sections);
+                    }
+                });
+    }
+
+    //填充list分类列表
+    private void fillSections(SectionList sections) {
+        RadioGroup.LayoutParams params=new RadioGroup.LayoutParams(getResources().getDimensionPixelOffset(R.dimen.filter_layout_left_view_tab_w),getResources().getDimensionPixelOffset(R.dimen.filter_layout_left_view_tab_h));
+        for (int i = 0; i <sections.size() ; i++) {
+            final Section section = sections.get(i);
+            final RadioButton radioButton= (RadioButton) View.inflate(this,R.layout.item_section_radiobtn,null);
+            radioButton.setLayoutParams(params);
+            radioButton.setText(section.title);
+            radioButton.setTag(section.url);
+            radioButton.setId(R.id.section_radiobtn+i);
+            radioButton.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if(hasFocus){
+                        radioButton.setPadding(40,0,40,0);
+                        radioButton.setGravity(Gravity.CENTER);
+                        radioButton.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+                    }else{
+                        radioButton.setPadding(40,0,75,0);
+                        radioButton.setGravity(Gravity.CENTER_VERTICAL|Gravity.RIGHT);
+                        radioButton.setEllipsize(TextUtils.TruncateAt.END);
+                    }
+                }
+            });
+            radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked){
+                        if(filterPopup!=null&&filterPopup.isShowing())
+                            filterPopup.dismiss();
+                        filter_checked_conditiion.setVisibility(View.INVISIBLE);
+                        filter_checked_conditiion.requestLayout();
+                        current_tab_arrow.setY(radioButton.getY()+getResources().getDimensionPixelOffset(R.dimen.filter_layout_left_view_tab_mt));
+                        fetchSectionData(section.url);
+                    }
+                }
+            });
+            if(i==sections.size()-1){
+                radioButton.setNextFocusDownId(R.id.section_radiobtn+i);
+            }
+            radioButton.setOnKeyListener(new View.OnKeyListener() {
+                @Override
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if(keyCode==22&&event.getAction()==KeyEvent.ACTION_DOWN){
+                        if(poster_recyclerview.getChildAt(0)!=null)
+                            poster_recyclerview.getChildAt(0).requestFocus();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            section_group.addView(radioButton);
+        }
+
+
+    }
+
+    //请求每个section的数据
+    private void fetchSectionData(String url) {
+        mSkyService.getItemListChannel(url)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseObserver<ItemList>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onNext(ItemList itemList) {
+                        processResultData(itemList);
+                    }
+                });
+
     }
 
     @Override
@@ -199,16 +312,16 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
         }
 
         if(keyCode==21){
-                if(filter_arrow_up.isFocused()){
-                    for (int i = 1; i <=spanCount ; i++) {
-                        if(poster_recyclerview.getChildAt(spanCount*2-i)!=null) {
-                            poster_recyclerview.getChildAt(spanCount*2-i).requestFocus();
-                            return true;
-                        }
+            if(filter_arrow_up.isFocused()){
+                for (int i = 1; i <=spanCount ; i++) {
+                    if(poster_recyclerview.getChildAt(spanCount*2-i)!=null) {
+                        poster_recyclerview.getChildAt(spanCount*2-i).requestFocus();
+                        return true;
                     }
-
                 }
+
             }
+        }
 
         return super.onKeyDown(keyCode, event);
     }
@@ -267,7 +380,7 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
             }
         });
         filterPopup.setBackgroundDrawable(getResources().getDrawable(R.drawable.transparent));
-        filterPopup.showAtLocation(getRootView(), Gravity.NO_GRAVITY, 0, getResources().getDimensionPixelOffset(R.dimen.filter_condition_popup_position));
+        filterPopup.showAtLocation(getRootView(), Gravity.BOTTOM, 0, 0);
         Message msg = new Message();
         msg.arg1 = -1;
         ((FilterConditionGroupView) filter_conditions.getChildAt(0)).handler.sendMessage(msg);
@@ -318,65 +431,68 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
 
                     @Override
                     public void onNext(final ItemList itemList) {
-                        filter_arrow_up.setVisibility(View.INVISIBLE);
-                        if(itemList.objects.size()!=0){
-                            if((isVertical&&itemList.objects.size()>10)||(!isVertical&&itemList.objects.size()>6)) {
-                                filter_arrow_down.setVisibility(View.VISIBLE);
-                            }else{
-                                filter_arrow_down.setVisibility(View.INVISIBLE);
-                            }
-                        }else{
-                            filter_arrow_down.setVisibility(View.INVISIBLE);
-                        }
-                        filterPosterAdapter = new FilterPosterAdapter(FilterActivity.this,itemList,isVertical);
-                        poster_recyclerview.swapAdapter(filterPosterAdapter,true);
-                        filterPosterAdapter.setItemClickListener(new OnItemClickListener() {
-                            @Override
-                            public void onItemClick(View view, int position) {
-                                baseSection="";
-                                PageIntent intent = new PageIntent();
-                                Item item=itemList.objects.get(position);
-                                if(item.content_model.contains("gather")){
-                                    intent.toSubject(FilterActivity.this,item.content_model,item.pk,item.title,Source.RETRIEVAL.getValue(),baseChannel);
-                                }else if(item.is_complex) {
-                                    intent.toDetailPage(FilterActivity.this,Source.RETRIEVAL.getValue(),item.pk);
-                                }else{
-                                    intent.toPlayPage(FilterActivity.this,item.pk,0, Source.RETRIEVAL);
-                                }
-                            }
-                        });
-                        filterPosterAdapter.setItemFocusedListener(new OnItemFocusedListener() {
-                            @Override
-                            public void onItemfocused(View view, int position, boolean hasFocus) {
-                                if(hasFocus){
-                                    isFocused = true;
-                                    focusedPos =poster_recyclerview.indexOfChild(view);
-                                    if(view.getY()>getResources().getDimensionPixelOffset(R.dimen.filter_poster_start_scroll_length)){
-                                        if(canScroll) {
-                                            poster_recyclerview.smoothScrollBy(0, (int) (view.getY() - view.getHeight() - getResources().getDimensionPixelOffset(R.dimen.filter_item_vertical_poster_mb)));
-                                        }else{
-                                            canScroll=true;
-                                        }
-                                    }
-                                    JasmineUtil.scaleOut3(view);
-                                    if(isVertical) {
-                                        view.findViewById(R.id.item_vertical_poster_title).setSelected(true);
-                                    }else {
-                                        view.findViewById(R.id.item_horizontal_poster_title).setSelected(true);
-                                    }
-                                }else{
-                                    JasmineUtil.scaleIn3(view);
-                                    if(isVertical) {
-                                        view.findViewById(R.id.item_vertical_poster_title).setSelected(false);
-                                    }else {
-                                        view.findViewById(R.id.item_horizontal_poster_title).setSelected(false);
-                                    }
-                                }
-                            }
-                        });
-
+                        processResultData(itemList);
                     }
                 });
+    }
+
+    private void processResultData(final ItemList itemList) {
+        filter_arrow_up.setVisibility(View.INVISIBLE);
+        if(itemList.objects.size()!=0){
+            if((isVertical&&itemList.objects.size()>10)||(!isVertical&&itemList.objects.size()>6)) {
+                filter_arrow_down.setVisibility(View.VISIBLE);
+            }else{
+                filter_arrow_down.setVisibility(View.INVISIBLE);
+            }
+        }else{
+            filter_arrow_down.setVisibility(View.INVISIBLE);
+        }
+        filterPosterAdapter = new FilterPosterAdapter(FilterActivity.this,itemList,isVertical);
+        poster_recyclerview.swapAdapter(filterPosterAdapter,true);
+        filterPosterAdapter.setItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                baseSection="";
+                PageIntent intent = new PageIntent();
+                Item item=itemList.objects.get(position);
+                if(item.content_model.contains("gather")){
+                    intent.toSubject(FilterActivity.this,item.content_model,item.pk,item.title,Source.RETRIEVAL.getValue(),baseChannel);
+                }else if(item.is_complex) {
+                    intent.toDetailPage(FilterActivity.this,Source.RETRIEVAL.getValue(),item.pk);
+                }else{
+                    intent.toPlayPage(FilterActivity.this,item.pk,0, Source.RETRIEVAL);
+                }
+            }
+        });
+        filterPosterAdapter.setItemFocusedListener(new OnItemFocusedListener() {
+            @Override
+            public void onItemfocused(View view, int position, boolean hasFocus) {
+                if(hasFocus){
+                    isFocused = true;
+                    focusedPos =poster_recyclerview.indexOfChild(view);
+                    if(view.getY()>getResources().getDimensionPixelOffset(R.dimen.filter_poster_start_scroll_length)){
+                        if(canScroll) {
+                            poster_recyclerview.smoothScrollBy(0, (int) (view.getY() - view.getHeight() - getResources().getDimensionPixelOffset(R.dimen.filter_item_vertical_poster_mb)));
+                        }else{
+                            canScroll=true;
+                        }
+                    }
+                    JasmineUtil.scaleOut3(view);
+                    if(isVertical) {
+                        view.findViewById(R.id.item_vertical_poster_title).setSelected(true);
+                    }else {
+                        view.findViewById(R.id.item_horizontal_poster_title).setSelected(true);
+                    }
+                }else{
+                    JasmineUtil.scaleIn3(view);
+                    if(isVertical) {
+                        view.findViewById(R.id.item_vertical_poster_title).setSelected(false);
+                    }else {
+                        view.findViewById(R.id.item_horizontal_poster_title).setSelected(false);
+                    }
+                }
+            }
+        });
     }
 
     private void filter() {
@@ -437,6 +553,10 @@ public class FilterActivity extends BaseActivity implements View.OnClickListener
         }else if(i==R.id.filter_tab){
             filter_tab.setFocusable(false);
             getRootView().requestFocus();
+            current_tab_arrow.setY(filter_tab.getY()+getResources().getDimensionPixelOffset(R.dimen.filter_layout_left_view_tab_mt));
+            if(filter_checked_conditiion.getChildCount()>1){
+                filter_checked_conditiion.setVisibility(View.VISIBLE);
+            }
             if(filterPopup!=null&&!filterPopup.isShowing()) {
                 showFilterPopup();
             }
