b3cd7f083f0653016976bee1a23bc29912cc7128
xidaokun <707906867@qq.com>
2017-08-31 14:28:34 +0800

    +横版双行布局及部分业务逻辑

---
+++ homepage/src/main/java/tv/ismar/homepage/adapter/DoubleLdAdapter.java
@@ -0,0 +1,47 @@
+package tv.ismar.homepage.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import tv.ismar.app.entity.banner.BannerPoster;
+
+/**
+ * @AUTHOR: xi
+ * @DATE: 2017/8/31
+ * @DESC: 横版双行适配器
+ */
+
+public class DoubleLdAdapter extends BaseAdapter{
+
+    private Context mContext;
+    private List<BannerPoster> mData;
+
+    public DoubleLdAdapter(Context context, List<BannerPoster> data){
+        this.mContext = context;
+        this.mData = data;
+    }
+
+    @Override
+    public int getCount() {
+        return (mData!=null) ? mData.size():0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return (mData!=null)? mData.get(position) : position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- homepage/src/main/java/tv/ismar/homepage/template/TemplateDoubleLd.java
+++ homepage/src/main/java/tv/ismar/homepage/template/TemplateDoubleLd.java
@@ -3,6 +3,14 @@ package tv.ismar.homepage.template;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import tv.ismar.app.BaseControl;
+import tv.ismar.app.entity.banner.HomeEntity;
+import tv.ismar.homepage.R;
+import tv.ismar.homepage.adapter.DoubleLdAdapter;
+import tv.ismar.homepage.control.GuideControl;
 
 /**
  * @AUTHOR: xi
@@ -10,18 +18,41 @@ import android.view.View;
  * @DESC: 横版双行模版
  */
 
-public class TemplateDoubleLd extends Template{
+public class TemplateDoubleLd extends Template implements BaseControl.ControlCallBack{
+    private ImageView mHorizontalImg;
+    private DoubleLdAdapter mAdapter;
+    private GuideControl mControl;
+    private GridView mGridView;
+
     public TemplateDoubleLd(Context context) {
         super(context);
+        mControl = new GuideControl(mContext, this);
     }
 
     @Override
     public void getView(View view) {
-
+        mHorizontalImg = (ImageView) view.findViewById(R.id.double_md_vertical_img);
     }
 
     @Override
     public void initData(Bundle bundle) {
+        mControl.fetchBannerList();
+    }
 
+    @Override
+    public void callBack(int flags, Object... args) {
+        if(flags == GuideControl.FETCH_BANNERS_LIST_FLAG){//获取单个banner业务
+            if(mAdapter == null){
+                HomeEntity homeEntity = (HomeEntity) args[0];
+                if(homeEntity != null){
+                    mAdapter = new DoubleLdAdapter(mContext, homeEntity.poster);
+                    mGridView.setAdapter(mAdapter);
+                }
+            }else {
+                mAdapter.notifyDataSetChanged();
+            }
+        } else if(flags == GuideControl.FETCH_M_BANNERS_LIST_FLAG){//获取多个banner业务
+
+        }
     }
 }
