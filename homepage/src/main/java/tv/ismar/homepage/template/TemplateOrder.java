299f4cedc828e73d0e518d9289c32195bb2ee75c
fenghuibin <fenghuibin@ismartv.cn>
2017-09-12 13:50:21 +0800

    修改分页加载

--- homepage/src/main/java/tv/ismar/homepage/template/TemplateOrder.java
+++ homepage/src/main/java/tv/ismar/homepage/template/TemplateOrder.java
@@ -10,6 +10,7 @@ import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.open.androidtvwidget.leanback.recycle.LinearLayoutManagerTV;
 import com.open.androidtvwidget.leanback.recycle.RecyclerViewTV;
+import com.orhanobut.logger.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +31,7 @@ import tv.ismar.homepage.control.OrderControl;
  * @DESC: 预约模版
  */
 
-public class TemplateOrder extends Template implements BaseControl.ControlCallBack{
+public class TemplateOrder extends Template implements BaseControl.ControlCallBack {
 
     private static final String TAG = "TemplateOrder";
     private OrderControl mControl;
@@ -39,66 +40,76 @@ public class TemplateOrder extends Template implements BaseControl.ControlCallBa
     private BannerSubscribeAdapter subscribeAdapter;
     private int mBannerName;
 
+    private boolean isInit = false;
+    private boolean isViewInit = false;
+
 //    private View subscribeArrowLeft;
 //    private View subscribeArrowRight;
 
     public TemplateOrder(Context context) {
         super(context);
+        Log.d(TAG, "TemplateOrder");
         mControl = new OrderControl(mContext, this);
     }
 
     @Override
     public void getView(View view) {
-        subscribeBanner = (RecyclerViewTV) view.findViewById(R.id.subscribe_banner);
-        LinearLayoutManagerTV subscribeLayoutManager = new LinearLayoutManagerTV(mContext, LinearLayoutManagerTV.HORIZONTAL, false);
-        int selectedItemSpace = mContext.getResources().getDimensionPixelSize(R.dimen.banner_item_SelectedItemSpace);
-//        subscribeBanner.addItemDecoration(new BannerSubscribeAdapter.SpacesItemDecoration(selectedItemSpace));
-        subscribeBanner.setLayoutManager(subscribeLayoutManager);
-        subscribeBanner.setSelectedItemAtCentered(false);
-        int selectedItemOffset = mContext.getResources().getDimensionPixelSize(R.dimen.banner_item_setSelectedItemOffset);
-        subscribeBanner.setSelectedItemOffset(selectedItemOffset, selectedItemOffset);
-        subscribeBanner.setPagingableListener(new RecyclerViewTV.PagingableListener() {
-            @Override
-            public void onLoadMoreItems() {
-                if (subscribeAdapter != null) {
-                    int currentPageNumber = subscribeAdapter.getCurrentPageNumber();
-                    if (currentPageNumber < subscribeAdapter.getTotalPageCount()) {
-                        fetchSubscribeBanner(mBannerName, currentPageNumber + 1);
+        Log.d(TAG, "getView");
+        if (!isViewInit) {
+            isViewInit = true;
+            subscribeBanner = (RecyclerViewTV) view.findViewById(R.id.subscribe_banner);
+            LinearLayoutManagerTV subscribeLayoutManager = new LinearLayoutManagerTV(mContext, LinearLayoutManagerTV.HORIZONTAL, false);
+            int selectedItemSpace = mContext.getResources().getDimensionPixelSize(R.dimen.banner_item_SelectedItemSpace);
+//            subscribeBanner.addItemDecoration(new BannerSubscribeAdapter.SpacesItemDecoration(selectedItemSpace));
+            subscribeBanner.setLayoutManager(subscribeLayoutManager);
+            subscribeBanner.setSelectedItemAtCentered(false);
+            int selectedItemOffset = mContext.getResources().getDimensionPixelSize(R.dimen.banner_item_setSelectedItemOffset);
+            subscribeBanner.setSelectedItemOffset(selectedItemOffset, selectedItemOffset);
+            subscribeBanner.setPagingableListener(new RecyclerViewTV.PagingableListener() {
+                @Override
+                public void onLoadMoreItems() {
+                    Log.d(TAG, "onLoadMoreItems" + subscribeAdapter);
+                    Log.d(TAG, "onLoadMoreItems" + subscribeAdapter.getCurrentPageNumber());
+                    Log.d(TAG, "onLoadMoreItems" + subscribeAdapter.getTotalPageCount());
+                    if (subscribeAdapter != null) {
+                        int currentPageNumber = subscribeAdapter.getCurrentPageNumber();
+                        if (currentPageNumber < subscribeAdapter.getTotalPageCount()) {
+                            fetchSubscribeBanner(mBannerName, currentPageNumber + 1);
+                        }
                     }
                 }
-            }
-        });
+            });
 
-        subscribeLayoutManager.setFocusSearchFailedListener(new LinearLayoutManagerTV.FocusSearchFailedListener() {
-            @Override
-            public View onFocusSearchFailed(View view, int focusDirection, RecyclerView.Recycler recycler, RecyclerView.State state) {
-                if (focusDirection == View.FOCUS_RIGHT || focusDirection == View.FOCUS_LEFT) {
-                    Log.d(TAG, "onFocusSearchFailed");
-                    if (subscribeBanner.getChildAt(0).findViewById(R.id.item_layout) == view ||
-                            subscribeBanner.getChildAt(subscribeBanner.getChildCount() - 1).findViewById(R.id.item_layout) == view) {
-                        YoYo.with(Techniques.HorizontalShake).duration(1000).playOn(view);
-                    } else {
+            subscribeLayoutManager.setFocusSearchFailedListener(new LinearLayoutManagerTV.FocusSearchFailedListener() {
+                @Override
+                public View onFocusSearchFailed(View view, int focusDirection, RecyclerView.Recycler recycler, RecyclerView.State state) {
+                    if (focusDirection == View.FOCUS_RIGHT || focusDirection == View.FOCUS_LEFT) {
+                        Log.d(TAG, "onFocusSearchFailed");
+                        if (subscribeBanner.getChildAt(0).findViewById(R.id.item_layout) == view ||
+                                subscribeBanner.getChildAt(subscribeBanner.getChildCount() - 1).findViewById(R.id.item_layout) == view) {
+                            YoYo.with(Techniques.HorizontalShake).duration(1000).playOn(view);
+                        } else {
 //                        if (focusDirection == View.FOCUS_RIGHT){
 //                            subscribeBanner.smoothScrollBy(10, 0);
 //                        }else if (focusDirection == View.FOCUS_LEFT){
 //                            subscribeBanner.smoothScrollBy(-10, 0);
 //                        }
+                        }
+                        Log.d(TAG, "onFocusSearchFailed: " + view);
+                        return view;
                     }
-                    Log.d(TAG, "onFocusSearchFailed: " + view);
-                    return view;
+                    return null;
                 }
-                return null;
-            }
-        });
+            });
 
-        subscribeBanner.setOnItemFocusChangeListener(new RecyclerViewTV.OnItemFocusChangeListener() {
-            @Override
-            public void onItemFocusGain(View itemView, int position) {
-                if (itemView != null && mContext != null && mTitleCountTv != null && subscribeAdapter != null) {
-                    mTitleCountTv.setText(String.format(mContext.getString(R.string.home_item_title_count), (position + 1) + "", subscribeAdapter.getTatalItemCount() + ""));
+            subscribeBanner.setOnItemFocusChangeListener(new RecyclerViewTV.OnItemFocusChangeListener() {
+                @Override
+                public void onItemFocusGain(View itemView, int position) {
+                    if (itemView != null && mContext != null && mTitleCountTv != null && subscribeAdapter != null) {
+                        mTitleCountTv.setText(String.format(mContext.getString(R.string.home_item_title_count), (position + 1) + "", subscribeAdapter.getTatalItemCount() + ""));
+                    }
                 }
-            }
-        });
+            });
 
 
 //        subscribeBanner.setOnHoverListener(new View.OnHoverListener() {
@@ -177,14 +188,17 @@ public class TemplateOrder extends Template implements BaseControl.ControlCallBa
 //                subscribeBanner.smoothScrollBy(400, 0);
 //            }
 //        });
-
+        }
     }
 
     @Override
     public void initData(Bundle bundle) {
 //        mControl.getBanners("overseasbanner", 1);
-        mBannerName = bundle.getInt("banner");
-        fetchSubscribeBanner(mBannerName, 1);
+        if (!isInit) {
+            isInit = true;
+            mBannerName = bundle.getInt("banner");
+            fetchSubscribeBanner(mBannerName, 1);
+        }
     }
 
 
@@ -195,7 +209,8 @@ public class TemplateOrder extends Template implements BaseControl.ControlCallBa
 
 
     private void fetchSubscribeBanner(int bannerName, final int pageNumber) {
-        if (pageNumber != 1){
+        Logger.d("fetchSubscribeBanner");
+        if (pageNumber != 1) {
             int startIndex = (pageNumber - 1) * 33;
             int endIndex;
             if (pageNumber == subscribeAdapter.getTotalPageCount()) {
@@ -235,9 +250,9 @@ public class TemplateOrder extends Template implements BaseControl.ControlCallBa
 
                     @Override
                     public void onNext(BannerEntity bannerEntity) {
-                        if (pageNumber == 1){
+                        if (pageNumber == 1) {
                             fillSubscribeBanner(bannerEntity);
-                        }else {
+                        } else {
                             int mSavePos = subscribeBanner.getSelectPostion();
                             subscribeAdapter.addDatas(bannerEntity);
 //                            mFocusHandler.sendEmptyMessageDelayed(mSavePos, 10);
@@ -247,6 +262,8 @@ public class TemplateOrder extends Template implements BaseControl.ControlCallBa
     }
 
     private void fillSubscribeBanner(BannerEntity bannerEntity) {
+//        Logger.d("fillSubscribeBanner");
+        Log.d(TAG, "fillSubscribeBanner");
         subscribeAdapter = new BannerSubscribeAdapter(mContext, bannerEntity.getPoster());
         subscribeAdapter.setSubscribeClickListener(new BannerSubscribeAdapter.OnBannerClickListener() {
             @Override
