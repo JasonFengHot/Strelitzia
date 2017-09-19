eae7db9a9aea938356253af58fea55146fef95a5
xidaokun <707906867@qq.com>
2017-09-19 18:20:42 +0800

    导视详情页跳转

--- homepage/src/main/java/tv/ismar/homepage/adapter/BaseViewHolder.java
+++ homepage/src/main/java/tv/ismar/homepage/adapter/BaseViewHolder.java
@@ -4,21 +4,35 @@ import android.animation.ObjectAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import tv.ismar.homepage.OnItemSelectedListener;
+
 import static android.view.View.SCALE_X;
 import static android.view.View.SCALE_Y;
 
 /**
  * @AUTHOR: xi
  * @DATE: 2017/9/15
- * @DESC: 说明
+ * @DESC: ViewHolder基类
  */
 public abstract class BaseViewHolder extends RecyclerView.ViewHolder implements
         View.OnFocusChangeListener, View.OnClickListener{
 
     public int mPosition;//item位置
+    private OnItemSelectedListener mClickListener = null;
 
-    public BaseViewHolder(View itemView) {
+    public BaseViewHolder(View itemView){
         super(itemView);
+        constructure();
+    }
+
+
+    public BaseViewHolder(View itemView, OnItemSelectedListener listener) {
+        super(itemView);
+        this.mClickListener = listener;
+        constructure();
+    }
+
+    private void constructure(){
         itemView.findViewById(getScaleLayoutId()).setOnFocusChangeListener(this);
         itemView.findViewById(getScaleLayoutId()).setOnClickListener(this);
     }
@@ -36,7 +50,9 @@ public abstract class BaseViewHolder extends RecyclerView.ViewHolder implements
 
     @Override
     public void onClick(View v) {
-
+        if(mClickListener!=null && v.getId()==getScaleLayoutId()){//item选中事件
+            mClickListener.itemSelected(v, mPosition);
+        }
     }
 
     /*缩放到1.1倍*/
