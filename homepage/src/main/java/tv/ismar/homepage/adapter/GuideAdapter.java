4d3c7c36c3dc5ad7e5f788f372e6ca99df736cb5
koujia <koujia@ismartv.cn>
2017-10-12 14:24:49 +0800

    修复首页crash问题

--- homepage/src/main/java/tv/ismar/homepage/adapter/GuideAdapter.java
+++ homepage/src/main/java/tv/ismar/homepage/adapter/GuideAdapter.java
@@ -45,7 +45,7 @@ public class GuideAdapter extends BaseRecycleAdapter<GuideAdapter.GuideViewHolde
 //        if(position==0) holder.mMarginLeftView.setVisibility(View.GONE);
         BannerPoster poster = mData.get(position);
         if (!TextUtils.isEmpty(poster.vertical_url)) {
-            if(poster.poster_url.equals("更多")){
+            if(poster.poster_url!=null&&poster.poster_url.equals("更多")){
                 Picasso.with(mContext).load(R.drawable.banner_vertical_more).into(holder.mPosterIg);
             } else {
                 Picasso.with(mContext).load(poster.vertical_url).into(holder.mPosterIg);
