af04f4dbc8a113d6c1353e07537f6a72df5447a9
liucan <liucan@ismartv.cn>
2017-10-31 10:28:30 +0800

    fix bug 4341

--- libismarplayer/src/main/java/tv/ismar/player/gui/PlaybackFragment.java
+++ libismarplayer/src/main/java/tv/ismar/player/gui/PlaybackFragment.java
@@ -2051,6 +2051,7 @@ public class PlaybackFragment extends Fragment implements PlaybackService.Client
                     baseActivity.showNoNetConnectDialog(null);
                 }
             } else if (baseActivity.isNoNetDialogShowing() && NetworkUtils.isConnected(context)) {
+                if(!baseActivity.isFinishing())
                 baseActivity.dismissNoNetConnectDialog();
                 mPlaybackService.resetPreload();
                 mPlaybackService.preparePlayer(extraItemPk, extraSubItemPk, extraSource);
