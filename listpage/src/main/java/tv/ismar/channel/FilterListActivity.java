09f3bccb02cce234d095c41037541a35209bd6d1
koujia <1452326017@qq.com>
2017-11-13 19:04:15 +0800

    修复列表页竖版翻页多翻一行的问题

--- listpage/src/main/java/tv/ismar/channel/FilterListActivity.java
+++ listpage/src/main/java/tv/ismar/channel/FilterListActivity.java
@@ -552,6 +552,8 @@ public class FilterListActivity extends BaseActivity implements View.OnClickList
                         if(isFirst){
                             isFirst=false;
                         }
+                        listSectionEntity.setObjects(listSectionEntity.getObjects().subList(0,1));
+                        listSectionEntity.setCount(1);
                         //将请求到的栏目数据替换之前占位的空数据
                         for (int i = 0; i <listSectionEntity.getObjects().size() ; i++) {
                             if(mAllSectionItemList.getCount()>specialPos.get(index)+i+1)
@@ -1232,7 +1234,7 @@ public class FilterListActivity extends BaseActivity implements View.OnClickList
                     mFilterFocusGridLayoutManager.scrollToPositionWithOffset(mFilterFocusGridLayoutManager.findLastVisibleItemPosition(), 0);
                 } else {
                     if (isVertical) {
-                        nextPos = mFocusGridLayoutManager.findLastVisibleItemPosition() + 1;
+                        nextPos = mFocusGridLayoutManager.findLastCompletelyVisibleItemPosition() + 1;
                         if(nextPos>mAllSectionItemList.getCount()-1){
                             nextPos=mFocusGridLayoutManager.findLastVisibleItemPosition();
                         }
